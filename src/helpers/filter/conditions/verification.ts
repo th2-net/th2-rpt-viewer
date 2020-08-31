7d8e7f0aee75d71baf60fb305a063685a3f06f46
ivan.druzhinin <ivan.druzhinin@exactprosystems.com>
2020-08-31 14:17:19 +0300

    [TH2-578] Correct copyright

--- src/helpers/filter/conditions/verification.ts
+++ src/helpers/filter/conditions/verification.ts
@@ -1,6 +1,6 @@
 
 /** ****************************************************************************
- * Copyright 2009-2020 Exactpro (Exactpro Systems Limited)
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
