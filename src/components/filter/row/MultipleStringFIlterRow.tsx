ef7936b09a4333d0c24e3d9ebd9e72a35ed19b80
vitaly mashanov <vitaly.mashanov@exactprosystems.com>
2020-11-12 16:42:43 +0300

    TH2-1000. Fix multiple strings input overflow

--- src/components/filter/row/MultipleStringFIlterRow.tsx
+++ src/components/filter/row/MultipleStringFIlterRow.tsx
@@ -20,7 +20,7 @@ import { removeByIndex, replaceByIndex } from '../../../helpers/array';
 import Bubble from '../../util/Bubble';
 import AutocompleteInput from '../../util/AutocompleteInput';
 import KeyCodes from '../../../util/KeyCodes';
-import { createBemElement } from '../../../helpers/styleCreators';
+import { createBemElement, createStyleSelector } from '../../../helpers/styleCreators';
 
 export default function MultipleStringFilterRow({
 	config,
@@ -71,9 +71,9 @@ export default function MultipleStringFilterRow({
 		input.current?.focus();
 	};
 
-	const inputRootClassName = createBemElement(
-		'filter-row',
-		'multiple-values',
+	const inputRootClassName = createBemElement('filter-row', 'multiple-values');
+	const filterContentClassName = createStyleSelector(
+		'filter-content',
 		isFocused || config.values.length > 0 ? 'active' : null,
 	);
 
@@ -82,40 +82,44 @@ export default function MultipleStringFilterRow({
 			<label className='filter-row__label' htmlFor={config.id}>
 				{config.label}
 			</label>
-			<div className={`${inputRootClassName} filter-row__input`} onClick={rootOnClick}>
-				{config.values.map((value, index) => (
-					<Bubble
-						key={index}
-						size='small'
-						removeIconType='white'
+			<div className={filterContentClassName}>
+				<div className={`${inputRootClassName} filter-row__input`} onClick={rootOnClick}>
+					{config.values.map((value, index) => (
+						<Bubble
+							key={index}
+							size='small'
+							removeIconType='white'
+							submitKeyCodes={[KeyCodes.SPACE, KeyCodes.ENTER]}
+							className='filter__bubble'
+							value={value}
+							onSubmit={valueBubbleOnChangeFor(index)}
+							onRemove={valueBubbleOnRemoveFor(index)}
+							isValid={
+								config.autocompleteList ? config.autocompleteList.includes(value) : undefined
+							}
+						/>
+					))}
+					{!isFocused && config.values.length === 0 ? (
+						<span className='filter-row__placeholder'>
+							Use Space to separate different words & Tab to finish
+						</span>
+					) : null}
+					<AutocompleteInput
+						ref={input}
 						submitKeyCodes={[KeyCodes.SPACE, KeyCodes.ENTER]}
-						className='filter__bubble'
-						value={value}
-						onSubmit={valueBubbleOnChangeFor(index)}
-						onRemove={valueBubbleOnRemoveFor(index)}
-						isValid={config.autocompleteList ? config.autocompleteList.includes(value) : undefined}
+						className='filter-row__multiple-values-input'
+						wrapperClassName='filter-row__multiple-values-input-wrapper'
+						value={config.currentValue}
+						autoresize
+						autocomplete={config.autocompleteList}
+						datalistKey={`autocomplete-${1}`}
+						onSubmit={inputOnSubmit}
+						onRemove={inputOnRemove}
+						onFocus={() => setIsFocused(true)}
+						onBlur={() => setIsFocused(false)}
+						inputStyle={{ width: '100%' }}
 					/>
-				))}
-				{!isFocused && config.values.length === 0 ? (
-					<span className='filter-row__placeholder'>
-						Use Space to separate different words & Tab to finish
-					</span>
-				) : null}
-				<AutocompleteInput
-					ref={input}
-					submitKeyCodes={[KeyCodes.SPACE, KeyCodes.ENTER]}
-					className='filter-row__multiple-values-input'
-					wrapperClassName='filter-row__multiple-values-input-wrapper'
-					value={config.currentValue}
-					autoresize
-					autocomplete={config.autocompleteList}
-					datalistKey={`autocomplete-${1}`}
-					onSubmit={inputOnSubmit}
-					onRemove={inputOnRemove}
-					onFocus={() => setIsFocused(true)}
-					onBlur={() => setIsFocused(false)}
-					inputStyle={{ width: '100%' }}
-				/>
+				</div>
 			</div>
 		</div>
 	);
