f2fdb8934107e6ab2946b0cfb949a249abde18de
Vitaly Mashanov <vitaly.mashanov@exactprosystems.com>
2020-12-07 14:31:16 +0300

    TH2-1133. Implement SSE client  * SSE store added

---
+++ src/stores/SSEStore.ts
@@ -0,0 +1,54 @@
+/** ****************************************************************************
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************** */
+
+import { action, observable } from 'mobx';
+import { createURLSearchParams } from '../helpers/url';
+import { EventTreeNode } from '../models/EventAction';
+import { EventMessage } from '../models/EventMessage';
+
+class SSEStore {
+	@observable events: EventTreeNode[] = [];
+
+	@observable messages: EventMessage[] = [];
+
+	@observable eventsChannel: EventSource | null = null;
+
+	@observable messagesChannel: EventSource | null = null;
+
+	@action startEventsChannel(
+		queryParams: Record<string, string | number | boolean | null | string[]>,
+	) {
+		const params = createURLSearchParams(queryParams);
+		this.eventsChannel = new EventSource(`backend/search/sse/events/?${params}`);
+	}
+
+	@action closeEventsChannel() {
+		this.eventsChannel?.close();
+	}
+
+	@action startMessagesChannel(
+		queryParams: Record<string, string | number | boolean | null | string[]>,
+	) {
+		const params = createURLSearchParams(queryParams);
+		this.eventsChannel = new EventSource(`backend/search/sse/events/?${params}`);
+	}
+
+	@action closeMessagesChannel() {
+		this.eventsChannel?.close();
+	}
+}
+
+export default SSEStore;
