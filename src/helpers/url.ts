9fd3b51ad3c372a1b16bb0b582a7ffc4c8789306
igor razvodov <igor.razvodov@exactprosystems.com>
2021-03-02 16:59:41 +0300

    Fix url replacing

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -113,6 +113,6 @@ export function registerUrlMiddleware(rootStore: RootStore) {
 				: undefined,
 		);
 		localStorageWorker.setLastSearchQuery(searchParams.toString());
-		window.history.replaceState({}, '', '/');
+		window.history.replaceState({}, '', window.location.pathname);
 	});
 }
