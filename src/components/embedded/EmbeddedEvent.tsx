e7b0df853a2424e32bc243c8d45e61926afdffbd
Ryzhkov-Alexey <alexey.ryzhkov@exactprosystems.com>
2021-07-30 14:01:25 +0300

    Added overflow handling for events and licensing

--- src/components/embedded/EmbeddedEvent.tsx
+++ src/components/embedded/EmbeddedEvent.tsx
@@ -1,3 +1,19 @@
+/** ****************************************************************************
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************** */
+
 import React, { useEffect, useState } from 'react';
 import { EventAction } from '../../models/EventAction';
 import EventBodyCard from '../event/EventBodyCard';
@@ -26,7 +42,7 @@ function EmbeddedEvent({ eventId }: { eventId: string }) {
 
 	if (event) {
 		return (
-			<>
+			<div className='embedded-wrapper'>
 				{Array.isArray(event.body) ? (
 					event.body.map((bodyPayloadItem, index) => (
 						<EventBodyCard
@@ -38,7 +54,7 @@ function EmbeddedEvent({ eventId }: { eventId: string }) {
 				) : (
 					<EventBodyCard key={eventId} body={event.body} parentEvent={event} />
 				)}
-			</>
+			</div>
 		);
 	}
 	return <SplashScreen />;
