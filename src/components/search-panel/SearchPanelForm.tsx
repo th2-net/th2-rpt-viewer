b31c413d3a83c4b9e53e6c1a62974ab0d2016d35
VitalyMashanov <vitaly.mashanov@exactprosystems.com>
2021-08-30 11:08:42 +0300

    TH2-2183.  * article 'the' added

--- src/components/search-panel/SearchPanelForm.tsx
+++ src/components/search-panel/SearchPanelForm.tsx
@@ -104,7 +104,7 @@ const SearchPanelForm = () => {
 		setValue: getFormStateUpdater('parentEvent'),
 		type: 'event-resolver',
 		id: 'parent-event',
-		placeholder: 'matches events by specified parent event id',
+		placeholder: 'matches events by the specified parent event id',
 	};
 
 	const messagesFormTypeConfig: FitlerRowItem = {
