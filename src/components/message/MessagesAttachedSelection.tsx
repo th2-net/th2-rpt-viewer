de6e57f1870b60158b4beed21b54beefb1248cc4
osinkinroman <osinkinroman@gmail.com>
2021-11-02 17:47:02 +0400

    useEffect fix

--- src/components/message/MessagesAttachedSelection.tsx
+++ src/components/message/MessagesAttachedSelection.tsx
@@ -1,21 +1,32 @@
+/** ****************************************************************************
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************** */
+
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useMessagesWorkspaceStore } from '../../hooks';
-import { EventMessage } from '../../models/EventMessage';
 
 const MessageAttachedSelection = () => {
 	const messagesStore = useMessagesWorkspaceStore();
 
 	const { attachedMessages } = messagesStore;
 
-	const [currentAttachedMessages, setCurrentAttachedMessages] = React.useState<EventMessage[]>([]);
 	const [messageIndex, setMessageIndex] = React.useState<number>(0);
 
 	React.useEffect(() => {
-		if (attachedMessages !== currentAttachedMessages) {
-			setCurrentAttachedMessages(attachedMessages);
-			setMessageIndex(0);
-		}
+		setMessageIndex(0);
 	}, [attachedMessages]);
 
 	const onPrevious = () => {
