52eb1e66eb6392f8260aacc9f0662912ed48781c
molotgor <molotgog@gmail.com>
2021-11-09 10:41:26 +0400

    added flatten list

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -42,7 +42,7 @@ module.exports = webpackMerge(commonConfig, {
 		historyApiFallback: true,
 		proxy: {
 			'/': {
-				target: 'http://th2-kube-demo:30000/th2-hand/',
+				target: 'http://th2-qa:30000/th2-commonv3/',
 				changeOrigin: true,
 				secure: false,
 			},
