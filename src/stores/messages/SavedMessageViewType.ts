8d246b8f06e0cbf084d802abaee216eb9c59f29f
tesohi <tesohi99@gmail.com>
2022-09-15 18:22:43 +0300

    fixed displaying messages with raw type

--- src/stores/messages/SavedMessageViewType.ts
+++ src/stores/messages/SavedMessageViewType.ts
@@ -66,7 +66,7 @@ export class SavedMessageViewType {
 
 	@action
 	private getViewTypes = () => {
-		this.viewTypes.set(this.message.id, this.displayRule);
+		this.viewTypes.set(this.message.id, getRawViewType(this.displayRule));
 		if (this.message.parsedMessages) {
 			this.message.parsedMessages.forEach(parsedMessage => {
 				if (this.isDisplayRuleRaw) {
