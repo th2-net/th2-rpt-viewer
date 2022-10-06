0a1d4983de83f32c177b62c33c7cf7e4144bc2a6
Igor Razvodov <igor.razvodov@exactprosystems.com>
2022-10-06 10:59:47 +0300

    Add sse retries

--- src/helpers/fetchRetry.ts
+++ src/helpers/fetchRetry.ts
@@ -1,5 +1,28 @@
-import fetchRet from 'fetch-retry';
+import fetchRetry from 'fetch-retry';
+import { nanoid } from 'nanoid';
+import notificationsStore from '../stores/NotificationsStore';
 
-export default function (input: RequestInfo, init?: RequestInit) {
-	return fetchRet(fetch)(input, { retries: 5 });
+export default function (input: RequestInfo) {
+	return fetchRetry(fetch)(input, {
+		retries: 3,
+		retryOn: async (attempt, error, response) => {
+			const retry = attempt < 2 ? error !== null : false;
+
+			if (!retry) {
+				if (response) {
+					notificationsStore.handleRequestError(response);
+				} else {
+					const url = typeof input === 'string' ? input : input.url;
+					notificationsStore.addMessage({
+						id: nanoid(),
+						notificationType: 'genericError',
+						header: 'Something went wrong while loading data',
+						description: url,
+						type: 'error',
+					});
+				}
+			}
+			return retry;
+		},
+	});
 }
