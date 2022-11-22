a4a67d5499e4a333c87678d34ec04ee3ba00cfed
Igor Razvodov <igor.razvodov@exactprosystems.com>
2022-11-22 16:29:44 +0300

    Events list navigation

---
+++ src/components/icons/FilterIcon.tsx
@@ -0,0 +1,26 @@
+/** *****************************************************************************
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************** */
+
+/* eslint-disable max-len */
+import { Icon, IconProps } from './Icon';
+
+export const FilterIcon = (props: IconProps) => (
+	<Icon {...props}>
+		<svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<path d='M10 18H14V16H10V18ZM3 6V8H21V6H3ZM6 13H18V11H6V13Z' fill='#585858' />
+		</svg>
+	</Icon>
+);
