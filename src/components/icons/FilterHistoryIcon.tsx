5021a06c4fbaeea8efc56791bec431fc12f6753e
Sergey <sergey.nuzhnyy@exactprosystems.com>
2022-11-29 07:08:53 +0000

    filters history icon added

---
+++ src/components/icons/FilterHistoryIcon.tsx
@@ -0,0 +1,33 @@
+/** *****************************************************************************
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************** */
+
+/* eslint-disable max-len */
+import { Icon, IconProps } from './Icon';
+
+export const FilterHistoryIcon = (props: IconProps) => (
+	<Icon {...props}>
+		<svg width='36' height='38' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<path
+				d='M10 3.75156C13.682 3.75156 16.6667 6.73622 16.6667 10.4182C16.6667 14.1002 13.682 17.0849 10 17.0849C6.318 17.0849 3.33334 14.1002 3.33334 10.4182H4.66667C4.66667 13.3636 7.05467 15.7516 10 15.7516C12.9453 15.7516 15.3333 13.3636 15.3333 10.4182C15.3333 7.47289 12.9453 5.08489 10 5.08489C8.35734 5.08489 6.888 5.82756 5.91 6.99489L7.33334 8.41822H3.33334V4.41822L4.96467 6.04889C6.18667 4.64222 7.98934 3.75156 10 3.75156ZM10.6667 7.08489V10.1416L12.8287 12.3036L11.8853 13.2469L9.33334 10.6936V7.08489H10.6667Z'
+				fill='currentColor'
+			/>
+			<path
+				d='M15 18.9182L12 14.4182V13.4182H20V14.4182L17 18.9182V21.9182L15 22.9182V18.9182Z'
+				fill='currentColor'
+			/>
+		</svg>
+	</Icon>
+);
