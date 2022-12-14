6eeabecd9a7fdf89367a69a081c47c2c8a09660d
Osinkin Roman <52595110+osinkinroman@users.noreply.github.com>
2022-12-14 12:24:30 +0400

    [TH2-4479] messages are shuffled when History window is used for searching (#495)
    * fixed sort of messages when timestamp equals
    
    * some improvements
    
    * simplified load logic
    
    * more changes
    
    * bump version
    
    * more changes
    
    * made requested changes
    
    * made requested changes
    
    * build fix
    
    * Update package.json
    
    * made requested changes
    
    * made requested changes

--- src/stores/messages/MessagesDataProviderStore.ts
+++ src/stores/messages/MessagesDataProviderStore.ts
@@ -27,14 +27,10 @@ import MessagesUpdateStore from './MessagesUpdateStore';
 import { MessagesDataStore } from '../../models/Stores';
 import { DirectionalStreamInfo } from '../../models/StreamInfo';
 import { extractMessageIds } from '../../helpers/streamInfo';
-import { isEventMessage } from '../../helpers/event';
-import { timestampToNumber } from '../../helpers/date';
 
 const FIFTEEN_SECONDS = 15 * 1000;
 
 export default class MessagesDataProviderStore implements MessagesDataStore {
-	private readonly messagesLimit = 250;
-
 	constructor(private messagesStore: MessagesStore, private api: ApiSchema) {
 		this.updateStore = new MessagesUpdateStore(this, this.messagesStore);
 
@@ -196,12 +192,7 @@ export default class MessagesDataProviderStore implements MessagesDataStore {
 		]);
 
 		runInAction(() => {
-			const messages = [
-				...nextMessages.filter(val => val.messageId !== message?.messageId),
-				...[message].filter(isEventMessage),
-				...prevMessages,
-			].sort((a, b) => timestampToNumber(b.timestamp) - timestampToNumber(a.timestamp));
-			this.messages = messages;
+			this.messages = [...nextMessages, ...prevMessages];
 		});
 
 		if (!this.messagesStore.selectedMessageId) {
@@ -297,16 +288,9 @@ export default class MessagesDataProviderStore implements MessagesDataStore {
 		}
 
 		if (messages.length) {
-			let newMessagesList = [
-				...this.messages,
-				...messages.sort((a, b) => timestampToNumber(b.timestamp) - timestampToNumber(a.timestamp)),
-			];
+			this.startIndex += messages.length;
 
-			if (newMessagesList.length > this.messagesLimit) {
-				newMessagesList = newMessagesList.slice(-this.messagesLimit);
-			}
-
-			this.messages = [...new Map(newMessagesList.map(item => [item.messageId, item])).values()];
+			this.messages = [...this.messages, ...messages];
 		}
 	};
 
@@ -328,37 +312,14 @@ export default class MessagesDataProviderStore implements MessagesDataStore {
 	public onNextChannelResponse = (messages: EventMessage[]) => {
 		this.lastNextChannelResponseTimestamp = null;
 
-		// eslint-disable-next-line no-param-reassign
-		messages = messages.filter(
-			msg => msg.messageId !== this.messagesStore.selectedMessageId?.valueOf(),
+		const nextMessages = messages.filter(
+			message => !this.messages.find(msg => msg.messageId === message.messageId),
 		);
 
-		const prevMessages =
-			this.messages.length > 0
-				? messages
-						.filter(
-							message =>
-								timestampToNumber(message.timestamp) <
-								timestampToNumber(this.messages[0].timestamp),
-						)
-						.sort((a, b) => timestampToNumber(b.timestamp) - timestampToNumber(a.timestamp))
-				: [];
-
-		const nextMessages = messages
-			.slice(0, messages.length - prevMessages.length)
-			.sort((a, b) => timestampToNumber(b.timestamp) - timestampToNumber(a.timestamp));
-
-		if (prevMessages.length > 0 || nextMessages.length > 0) {
+		if (nextMessages.length) {
 			this.startIndex -= this.updateStore.isActive ? 0 : nextMessages.length;
 
-			let newMessagesList = prevMessages.length
-				? [...nextMessages, this.messages[0], ...prevMessages, ...this.messages.slice(1)]
-				: [...nextMessages, ...this.messages];
-
-			if (newMessagesList.length > this.messagesLimit) {
-				newMessagesList = newMessagesList.slice(0, this.messagesLimit);
-			}
-			this.messages = [...new Map(newMessagesList.map(item => [item.messageId, item])).values()];
+			this.messages = [...nextMessages, ...this.messages];
 		}
 	};
 
