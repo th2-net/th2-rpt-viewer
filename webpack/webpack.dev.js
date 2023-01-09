7b129d1bc8b036a1cca17469aaff757b3be4d7c3
molotgor <molotgog@gmail.com>
2023-01-09 11:09:13 +0400

    changed proxy target

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -42,7 +42,7 @@ module.exports = webpackMerge(commonConfig, {
 		historyApiFallback: true,
 		proxy: {
 			'/': {
-				target: 'http://th2-kuber-test03:30000/th2-3822-books-pages/',
+				target: 'http://th2-kuber-test03:30000/th2-demo-books/',
 				changeOrigin: true,
 				secure: false,
 			},
