7725aa2be7a499c24927b1fcaeb093bb3714e3a0
Osinkin Roman <52595110+osinkinroman@users.noreply.github.com>
2023-02-27 13:00:30 +0400

    [TH2-4673] New messages not loading when scrolling up in message bar (#543)
    * now startTimestamp can't be more than current time
    
    * bump version
    
    * Math.floor to choosenTimestamp

--- src/components/graph/GraphChunksVirtualizer.tsx
+++ src/components/graph/GraphChunksVirtualizer.tsx
@@ -510,11 +510,13 @@ function TimeSelector(props: TimeSelectorProps) {
 		const withoutOff = e.pageX - (pointerEl?.offsetWidth || 6) / 2;
 		const offset = e.clientX - startOffset.current;
 		const x0 = (panelRange[0] + panelRange[1]) / 2;
-		if (Math.abs(offset) > 10) {
-			onClick(Math.floor(x0 + getTimeOffset(offset) - windowTimeRange[0]));
-		} else {
-			onClick(Math.floor(getTimeOffset(withoutOff)));
-		}
+		const choosenTimestamp = Math.floor(
+			Math.abs(offset) > 10
+				? x0 + getTimeOffset(offset) - windowTimeRange[0]
+				: getTimeOffset(withoutOff),
+		);
+
+		onClick(Math.min(choosenTimestamp, moment.utc().valueOf()));
 		startOffset.current = 0;
 		setX(0);
 		document.removeEventListener('mousemove', handleMouseDrag);
