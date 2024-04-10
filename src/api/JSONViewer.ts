14f5effbafb864c85fa692935ff70fd85ac7cda8
molotgor <molotgog@gmail.com>
2024-04-10 12:28:52 +0400

    change fetch path to json-stream-provider

--- src/api/JSONViewer.ts
+++ src/api/JSONViewer.ts
@@ -3,7 +3,7 @@ import { JSONViewerApiSchema } from './ApiSchema';
 
 const JSONViewerHttpApi: JSONViewerApiSchema = {
 	getLinks: async () => {
-		const res = await fetch(`backend/files/all`, {
+		const res = await fetch(`json-stream-provider/files/all`, {
 			cache: 'reload',
 			headers: {
 				Accept: 'application/json',
@@ -16,7 +16,7 @@ const JSONViewerHttpApi: JSONViewerApiSchema = {
 		return [];
 	},
 	getFile: async (path: string) => {
-		const res = await fetch(`backend/result?path=${path}`, {
+		const res = await fetch(`json-stream-provider/result?path=${path}`, {
 			headers: {
 				Accept: 'application/json',
 			},
@@ -29,7 +29,7 @@ const JSONViewerHttpApi: JSONViewerApiSchema = {
 		return {};
 	},
 	getParameters: async (path: string) => {
-		const res = await fetch(`backend/files?path=${path}`, {
+		const res = await fetch(`json-stream-provider/files?path=${path}`, {
 			headers: {
 				Accept: 'application/json',
 			},
@@ -42,7 +42,7 @@ const JSONViewerHttpApi: JSONViewerApiSchema = {
 		return {};
 	},
 	getResults: async (path: string) => {
-		const res = await fetch(`backend/result?path=${path}`);
+		const res = await fetch(`json-stream-provider/result?path=${path}`);
 		if (res.ok) {
 			return res.json();
 		}
@@ -50,7 +50,7 @@ const JSONViewerHttpApi: JSONViewerApiSchema = {
 		return path;
 	},
 	launchNotebook: async (path: string, parameters = {}) => {
-		const res = await fetch(`backend/execute?path=${path}`, {
+		const res = await fetch(`json-stream-provider/execute?path=${path}`, {
 			method: 'POST',
 			headers: {
 				'Content-type': 'application/json',
