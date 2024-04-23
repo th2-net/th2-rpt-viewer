0835687a9a121f4e7f9f6b73810e3ba658918d77
molotgor <molotgog@gmail.com>
2024-04-23 18:12:34 +0400

    removed deprecated escape in decoding of rawContent

--- src/components/message/message-card/raw/SimpleMessageRaw.tsx
+++ src/components/message/message-card/raw/SimpleMessageRaw.tsx
@@ -24,7 +24,7 @@ interface Props {
 export default function SimpleMessageRaw({ rawContent }: Props) {
 	const contentRef = React.useRef<HTMLDivElement>(null);
 
-	const humanReadableContent = decodeURIComponent(escape(atob(rawContent)));
+	const humanReadableContent = decodeURIComponent(atob(rawContent));
 	const convertedArr = splitOnReadableParts(humanReadableContent);
 
 	return (
