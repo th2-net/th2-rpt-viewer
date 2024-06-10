f3a882ceb22eacef090341f46904ea8180158030
molotgor <molotgog@gmail.com>
2024-06-10 13:14:28 +0400

    change JSON type display in JSON Viewer

--- src/components/JSONViewer/TreePanel.tsx
+++ src/components/JSONViewer/TreePanel.tsx
@@ -25,27 +25,6 @@ const TreePanel = ({
 		return 'no display name';
 	}, [treeNode.displayName, treeNode.key, treeNode.isGeneratedKey]);
 
-	if (
-		!(treeNode.isRoot && treeNode.isGeneratedKey) &&
-		(viewType === TreeViewType.JSON || viewType === TreeViewType.PRETTY)
-	) {
-		return (
-			<>
-				<div style={{ width: `${20 * nest + 23}px` }} />
-				<div className='message-card-wrapper'>
-					<div className='mc__mc-body mc-body'>
-						<JSONView isBeautified={viewType === TreeViewType.PRETTY} node={treeNode} />
-					</div>
-					<LeafTools
-						activeViewType={viewType}
-						toggleViewType={setViewType}
-						viewTypes={[TreeViewType.EVENTS_LIST, TreeViewType.JSON, TreeViewType.PRETTY]}
-					/>
-				</div>
-			</>
-		);
-	}
-
 	const complexFieldsDisplay = () => (
 		<span title={treeNode.isArray ? 'Complex Elements amount' : `Complex Fields amount`}>
 			{treeNode.isArray ? '[' : '{'}
@@ -102,45 +81,58 @@ const TreePanel = ({
 
 	return (
 		<>
-			<div className='leafWrapper'>
-				<div style={{ width: `${20 * nest + (treeNode.complexFields.length === 0 ? 23 : 0)}px` }} />
-				{treeNode.complexFields.length > 0 && (
+			<div
+				className={createBemBlock(
+					'leaf',
+					viewType === TreeViewType.JSON || viewType === TreeViewType.PRETTY ? 'expanded' : null,
+					treeNode.id === JSONViewerStore.selectedTreeNode.id ? 'selected' : null,
+				)}>
+				<div className='leafWrapper'>
 					<div
-						className={createBemBlock('expand-icon', open ? 'expanded' : 'hidden')}
-						onClick={() => setOpen(!open)}
+						style={{ width: `${20 * nest + (treeNode.complexFields.length === 0 ? 23 : 0)}px` }}
 					/>
-				)}
-				<div
-					className={createBemBlock(
-						'valueLeaf',
-						treeNode.id === JSONViewerStore.selectedTreeNode.id ? 'selected' : null,
-					)}
-					title={nodeName}
-					onClick={() => {
-						JSONViewerStore.selectTreeNode(treeNode);
-					}}>
-					<div
-						style={{
-							display: 'flex',
-							gap: 5,
-							alignItems: 'center',
-						}}>
-						<div className={createBemBlock('event-status-icon')} />
-						<span style={{ color: treeNode.isGeneratedKey ? '#333333' : undefined }}>
-							{nodeName}
-						</span>{' '}
-						<span style={{ color: '#333333' }}>
-							{complexFieldsDisplay()} {simpleFieldsDisplay()}
-						</span>
-					</div>
-					{!(treeNode.isRoot && treeNode.isGeneratedKey) && (
-						<LeafTools
-							activeViewType={viewType}
-							toggleViewType={setViewType}
-							viewTypes={[TreeViewType.EVENTS_LIST, TreeViewType.JSON, TreeViewType.PRETTY]}
+					{treeNode.complexFields.length > 0 && (
+						<div
+							className={createBemBlock('expand-icon', open ? 'expanded' : 'hidden')}
+							onClick={() => setOpen(!open)}
 						/>
 					)}
+					<div
+						className={createBemBlock('valueLeaf')}
+						title={nodeName}
+						onClick={() => {
+							JSONViewerStore.selectTreeNode(treeNode);
+						}}>
+						<div
+							style={{
+								display: 'flex',
+								gap: 5,
+								alignItems: 'center',
+							}}>
+							<div className={createBemBlock('event-status-icon')} />
+							<span style={{ color: treeNode.isGeneratedKey ? '#333333' : undefined }}>
+								{nodeName}
+							</span>{' '}
+							<span style={{ color: '#333333' }}>
+								{complexFieldsDisplay()} {simpleFieldsDisplay()}
+							</span>
+						</div>
+						{!(treeNode.isRoot && treeNode.isGeneratedKey) && (
+							<LeafTools
+								activeViewType={viewType}
+								toggleViewType={setViewType}
+								viewTypes={[TreeViewType.EVENTS_LIST, TreeViewType.JSON, TreeViewType.PRETTY]}
+							/>
+						)}
+					</div>
 				</div>
+				{(viewType === TreeViewType.JSON || viewType === TreeViewType.PRETTY) && (
+					<div className='message-card-wrapper'>
+						<div className='mc__mc-body mc-body'>
+							<JSONView isBeautified={viewType === TreeViewType.PRETTY} node={treeNode} />
+						</div>
+					</div>
+				)}
 			</div>
 			{open &&
 				treeNode.complexFields.map(field => (
